5b5da14a7867043bb41dcb09d5ba6371f57ba770
mehmet can yolar <mehmetcanyolar@gmail.com>
2023-03-12 00:30:33 +0300

    sign up sayfası yapmak için register.html kodu yok ve @media tanımlanmadı hatası alıyorum bootstrapteki taslaktan

--- App.Web.Mvc1/Controllers/AuthController.cs
+++ App.Web.Mvc1/Controllers/AuthController.cs
@@ -4,9 +4,14 @@ namespace App.Web.Mvc1.Controllers
 {
 	public class AuthController : Controller
 	{
-		public IActionResult Index()
+		public IActionResult Kayitol()
 		{
 			return View();
 		}
-	}
+
+        public IActionResult Girisyap()
+        {
+            return View();
+        }
+    }
 }
