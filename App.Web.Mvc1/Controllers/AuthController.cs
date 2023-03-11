e6ccc975b5d82b5aab0e0be05f69a25d75a45c5e
mehmet can yolar <mehmetcanyolar@gmail.com>
2023-03-12 00:38:45 +0300

    register dosyası paylaşılmamış

--- App.Web.Mvc1/Controllers/AuthController.cs
+++ App.Web.Mvc1/Controllers/AuthController.cs
@@ -4,14 +4,16 @@ namespace App.Web.Mvc1.Controllers
 {
 	public class AuthController : Controller
 	{
-		public IActionResult Kayitol()
-		{
-			return View();
-		}
-
+	
         public IActionResult Girisyap()
         {
             return View();
         }
+
+        public IActionResult Kayitol()
+        {
+            return View();
+        }
+
     }
 }
